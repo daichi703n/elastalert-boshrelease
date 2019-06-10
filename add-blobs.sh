84223f3fc2c242b75917cfa47410f316aaf96e77
Toshiaki Maki <tmaki@pivotal.io>
2019-06-10 13:11:32 +0900

    Revert "Bump Elastalert and reset blobs"
    This reverts commit 7b2852713984775d717229206bb8a51b712c561d.

--- add-blobs.sh
+++ add-blobs.sh
@@ -20,8 +20,7 @@ blob_download() {
 blob_download python2.7 https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz Python-2.7.15.tgz
 blob_download libffi https://buildpacks.cloudfoundry.org/dependencies/manual-binaries/python/libffi-3.2.1-linux-x64-5f5bf32c.tgz libffi-3.2.1.tgz
 
-pip download -d elastalert --no-binary :all: elastalert==0.2.0b2
-
+pip download -d elastalert --no-binary :all: elastalert==0.1.35
 for f in $(ls elastalert/*.tar.gz);do 
   bosh add-blob --dir=${DIR} ${f} ${f}
 done
